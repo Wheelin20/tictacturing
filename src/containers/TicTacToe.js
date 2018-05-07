36be08f04839edd0818d52730a3753c379998ac8
Kevin Emigh <kevin@waldoms.com>
2018-05-06 23:19:19 -0400

    adds placeholders for TicTacToe and adds react-konva

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -2,11 +2,47 @@ import React, {Component} from 'react'
 
 class TicTacToe extends Component
 {
+    constructor(props)
+    {
+        super(props)
+
+        this.state =
+        {
+
+        }
+    }
+
+    componentWillMount()
+    {
+        // When my component mounts
+    }
+
+    move = () =>
+    {
+        // placeholder for player move
+    }
+
+    makeAiMove = () =>
+    {
+        // placeholder for Ai move
+    }
+
+    turingTest = () => 
+    {
+        // placeholder
+    }
+
+    recordGame = () =>
+    {
+
+    }
+
     render()
     {
         return (
             <div>
-                <h2>Home</h2>
+                {/* <Board /> */}
+                {/* <Squares /> */}
             </div>
         )
     }
