9088846f141a9e68d30f471579388c3b35bebc4b
Kevin Emigh <kevin@waldoms.com>
2018-05-08 19:07:10 -0400

    adds winning combinations and removes a log statement

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -19,6 +19,13 @@ class TicTacToe extends Component
             winner: false,
             win: false
         }
+
+        this.combos =
+        [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,5], [2,5,8],
+            [0,4,8], [2,4,6]
+        ]
     }
 
     componentWillMount()
@@ -82,8 +89,6 @@ class TicTacToe extends Component
             yourTurn,
             ownMark
         } = this.state
-
-        console.log(gameOver + " | " + this.state.gameOver)
 
         return (
             <div>
