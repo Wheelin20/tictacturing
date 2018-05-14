4efd0fe2a11d4259f9373d2d00fc7b190603c3a6
Kevin Emigh <kevin@waldoms.com>
2018-05-14 11:38:18 -0400

    starts adding relay queries

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
+import Relay from 'react-relay'
 import Template from '../containers/Template'
 import TicTacToe from '../containers/TicTacToe'
 import Profile from '../containers/Profile'
 
-const createRoutes = () => {
+const ViewQueries =
+{
+    view: () => Relay.QL`query { viewer }`
+}
+
+const createRoutes = () =>
+{
   return (
-    <Route path='/' component={Template}
-    >
+    <Route path='/' component={Template} query={ViewerQueries}>
       <IndexRoute component={TicTacToe} />
-      <Route path={'/profile'} component={Profile} />
+      <Route path={'/profile'} component={Profile} query={ViewerQueries} />
     </Route>
   )
 }
